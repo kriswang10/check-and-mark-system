a4db500ba4d8c688eda320a3c9c1ad624b8a59e7
Alevals <1847924694@qq.com>
2019-11-05 23:40:45 +0800

    Add files via upload

--- contract/js/main_pages.js
+++ contract/js/main_pages.js
@@ -1,12 +1,25 @@
-var fuzzy = document.getElementById("fuzzy");
-fuzzy.onmouseover = fuzzychange;
+var a = document.getElementById("bx").getElementsByTagName("p");
+var b = document.getElementById("gx").getElementsByTagName("div");
+for (var i = 0; i < a.length; i++){
+    a[i].onmouseover = shadowBack;
+    a[i].onmouseout = shadowBackout;
+}
+for (var i = 0; i < c.length; i++){
+    c[i].onmouseover = showLine;
+}
+function shadowBack() {
 
-function fuzzychange() {
-
-    if (fuzzy === this){
-        fuzzy.className += "filter";
+    for (var i = 0; i < b.length; i++){
+        if (a[i] === this) {
+            var t = i + 1;
+            b[i].className = "text-" + t + " filter";
+        }
     }
-    else {
-        fuzzy.className = "filter";
+
+}
+function shadowBackout() {
+    for (var i = 0; i < b.length; i++){
+        var t = i + 1;
+        b[i].className = "text-" + t;
     }
 }
--- contract/js/switch.js
+++ contract/js/switch.js
@@ -1,9 +1,30 @@
-var tabs =document.getElementById("tabs").getElementsByTagName("li");
-console.log(tabs);
-for (var i = 0; i < tabs.length; i++){
-    tabs[i].onclick = backcolorchange;
+var a = document.getElementById("tabs").getElementsByTagName("li");
+var b = document.getElementById("fontColor").getElementsByTagName("span");
+for (var i = 0; i < a.length; i++){
+    b[i].onmouseover = showBack;
+    b[i].onmouseout = deshowBack;
 }
+function showBack() {
+    for (var i = 0; i < b.length; i++){
+        if (b[i] === this){
+            var t = i +1;
+            a[i].className = "menu-nav-" + t + " active-blue-back";
+            b[i].className = "active-font";
+        }
+        else {
+            var t = i +1;
+            a[i].className = "menu-nav-" + t;
+            b[i].className = "";
+        }
+    }
+}
+function deshowBack() {
+    for (var i = 0; i < b.length; i++) {
+        if (b[i] != this){
+            var t = i + 1;
+            a[i].className = "menu-nav-" + t;
+            b[i].className = "";
+        }
 
-function backcolorchange() {
-    tabs[i].className = "active-blue-back";
+    }
 }
